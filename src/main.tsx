3b3620aca5ddffa35f0b61eb494508b0dc8c381d
JayadityaGit <jgedela@gitam.in>
2025-06-08 11:48:31 +0530

    analv2

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { inject } from '@vercel/analytics'; // THIS IS THE KEY IMPORT
+
+// Call inject once early in your application
+inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
